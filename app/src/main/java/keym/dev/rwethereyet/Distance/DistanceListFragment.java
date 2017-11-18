efef80167f52b6ef638f398a7afc87a2cfd4cd70
luka <luka@Shane>
2017-11-18 19:37:29 +0100

    More on distance Fragment

--- app/src/main/java/keym/dev/rwethereyet/Distance/DistanceListFragment.java
+++ app/src/main/java/keym/dev/rwethereyet/Distance/DistanceListFragment.java
@@ -6,8 +6,15 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import keym.dev.rwethereyet.R;
+import keym.dev.rwethereyet.keym.dev.rwethereyet.util.LocationItem;
+import keym.dev.rwethereyet.keym.dev.rwethereyet.util.LocationParser;
+import keym.dev.rwethereyet.locations.LocationListAdapter;
 
 /**
  * Created by luka on 17/11/17.
@@ -17,6 +24,12 @@ public class DistanceListFragment extends Fragment {
 
     private static final String TAG = "DistanceListFragment";
 
+    private View rootView;
+    private List<LocationItem> locations;
+    private ListView distancesView;
+
+    private DistanceListAdapter adapter;
+
     @Override
     public View onCreateView(final LayoutInflater inflater,
                              final ViewGroup container,
@@ -25,7 +38,16 @@ public class DistanceListFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        this.rootView = view;
+
+        this.locations = new ArrayList<>();
+        this.distancesView = (ListView) this.rootView.findViewById(R.id.distanceList);
+
+        this.locations = new LocationParser(this.getContext()).readAllItems();
+
+        this.adapter = new DistanceListAdapter(this.getActivity(), R.layout.item_distance, this.locations);
+        this.distancesView.setAdapter(this.adapter);
     }
 }
